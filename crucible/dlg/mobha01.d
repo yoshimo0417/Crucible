d6a8003419d26448809ebf9558588148ffb5b324
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-11-28 12:32:07 -0700

    Bhaal's Kingdom Greeting

---
+++ crucible/dlg/mobha01.d
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////
+// Dialogue for Deathbringer
+// Greets Player in Bhaal's Kingdom
+///////////////////////////////////////////////////////////////////
+BEGIN MOBHA01
+
+IF ~Global("MO_DeathbringerShout","LOCALS",0)
+AreaCheck("mobha3")~ THEN BEGIN 0
+  SAY @6300
+  IF ~~ THEN DO ~SetGlobal("MO_DeathbringerShout","LOCALS",1)
+Global("MOBHA3_Enemy","GLOBAL",1)
+Enemy()~ EXIT
+END
+
