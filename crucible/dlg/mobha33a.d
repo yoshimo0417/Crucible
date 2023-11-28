ee163b4d19c19347563ae7f12fb72cb9d0ede264
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-11-28 00:18:17 -0700

    Dialog Stuff

--- crucible/dlg/mobha33a.d
+++ crucible/dlg/mobha33a.d
@@ -6,7 +6,13 @@ BEGIN MOBHA33
 
 IF ~~ THEN BEGIN 0
   SAY @6100
-  IF ~~ THEN REPLY @6101 DO ~ClearAllActions()
+  IF ~~ THEN REPLY @6101 GOTO 1
+  IF ~~ THEN REPLY @6102 GOTO 2
+END
+
+IF ~~ THEN BEGIN 1
+  SAY @6103
+  IF ~~ THEN DO ~ClearAllActions()
 SaveGame(2)
 ActionOverride(Player1,LeaveAreaLUA("MOBHA1","",[2518.1543],12))
 ActionOverride(Player2,LeaveAreaLUA("MOBHA1","",[2459.1512],12))
@@ -14,5 +20,9 @@ ActionOverride(Player3,LeaveAreaLUA("MOBHA1","",[2590.1620],12))
 ActionOverride(Player4,LeaveAreaLUA("MOBHA1","",[2400.1567],12))
 ActionOverride(Player5,LeaveAreaLUA("MOBHA1","",[2527.1696],12))
 ActionOverride(Player6,LeaveAreaLUA("MOBHA1","",[2464.1622],12))~ EXIT
-  IF ~~ THEN REPLY @6102 DO ~DestroySelf()~ EXIT
+END
+
+IF ~~ THEN BEGIN 2
+  SAY @6104
+  IF ~~ THEN DO ~DestroySelf()~ EXIT
 END
--- crucible/dlg/mobha33b.d
+++ crucible/dlg/mobha33b.d
@@ -6,10 +6,20 @@ BEGIN MOBHA33
 
 IF ~~ THEN BEGIN 0
   SAY @6100
-  IF ~~ THEN REPLY @6101 DO ~ClearAllActions()
+  IF ~~ THEN REPLY @6101 GOTO 1
+  IF ~~ THEN REPLY @6102 GOTO 2
+END
+
+IF ~~ THEN BEGIN 1
+  SAY @6103
+  IF ~~ THEN DO ~ClearAllActions()
     SaveGame(2)
     SmallWait(5)
     StartCutSceneMode()
     StartCutScene("cut248a")~ EXIT
-  IF ~~ THEN REPLY @6102 DO ~DestroySelf()~ EXIT
+END
+
+IF ~~ THEN BEGIN 2
+  SAY @6104
+  IF ~~ THEN DO ~DestroySelf()~ EXIT
 END
