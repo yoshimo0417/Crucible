1b141467e44eb825c7684179e429049b60ab639e
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-11-28 12:33:18 -0700

    Update mobha01.d

--- crucible/dlg/mobha01.d
+++ crucible/dlg/mobha01.d
@@ -8,7 +8,7 @@ IF ~Global("MO_DeathbringerShout","LOCALS",0)
 AreaCheck("mobha3")~ THEN BEGIN 0
   SAY @6300
   IF ~~ THEN DO ~SetGlobal("MO_DeathbringerShout","LOCALS",1)
-Global("MOBHA3_Enemy","GLOBAL",1)
+SetGlobal("MOBHA3_Enemy","GLOBAL",1)
 Enemy()~ EXIT
 END
 
