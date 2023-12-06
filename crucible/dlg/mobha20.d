b0bd68ad37354ecac2604faab8ff81f308584b56
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-06 12:21:30 -0700

    Added lich for final boss and phiuhl

--- crucible/dlg/mobha20.d
+++ crucible/dlg/mobha20.d
@@ -59,6 +59,7 @@ CreateCreatureEffect("mobha15","SPMETSWA",[1214.683],N)  // Demon Knight
 CreateCreatureEffect("mobha16","SPMETSWA",[1230.784],N)  // Lilitu
 CreateCreatureEffect("mobha16","SPMETSWA",[1184.747],N)  // Lilitu
 CreateCreatureEffect("mobha21","SPMETSWA",[527.667],SE)  // Succubus
+CreateCreatureEffect("mobha21","SPMETSWA",[1001.441],W)  // Succubus
 Enemy()~ EXIT
 END
 
