2ef2188079396cf890795a61b753850e9a43e41e
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-08 13:48:11 -0700

    Dying Deathbringer

--- crucible/dlg/mobha01d.d
+++ crucible/dlg/mobha01d.d
@@ -5,7 +5,59 @@
 BEGIN MOBHA01D
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
-  SAY @6500
-  IF ~~ THEN DO ~~ EXIT
+  SAY @6500 = @6501 = @6502
+  IF ~~ THEN REPLY @6505 GOTO 4  // Can I aid you?
+  IF ~~ THEN REPLY @6506 GOTO 5  // Who are you?
+  IF ~~ THEN REPLY @6507 GOTO 6  // There are bodies everywhere, what happened here?
+  IF ~~ THEN REPLY @6508 GOTO 7  // Where is Bhaal's kingdom and how do I enter it?
+  IF ~~ THEN REPLY @6509 GOTO 8  // What threats do I face here?
+  IF ~~ THEN REPLY @6503 GOTO 1  // Be at peace.
+  IF ~~ THEN REPLY @6504 GOTO 2  // I'll end your existence!
 END
 
+IF ~~ THEN BEGIN 1
+  SAY @6511
+  IF ~~ THEN DO ~DestroyItem("minhp1")
+Kill(Myself)~ EXIT
+END
+
+IF ~~ THEN BEGIN 2
+  SAY @6510
+  IF ~~ THEN GOTO 1
+END
+
+IF ~~ THEN BEGIN 3
+  SAY @6512
+  IF ~Global("MO_AidDeathbringer","GLOBAL",0)~ THEN REPLY @6505 GOTO 4  // Can I aid you?
+  IF ~~ THEN REPLY @6506 GOTO 5  // Who are you?
+  IF ~~ THEN REPLY @6507 GOTO 6  // There are bodies everywhere, what happened here?
+  IF ~~ THEN REPLY @6508 GOTO 7  // Where is Bhaal's kingdom and how do I enter it?
+  IF ~~ THEN REPLY @6509 GOTO 8  // What threats do I face here?
+  IF ~~ THEN REPLY @6503 GOTO 1  // Be at peace.
+  IF ~~ THEN REPLY @6504 GOTO 2  // I'll end your existence!
+END
+
+IF ~~ THEN BEGIN 4
+  SAY @6520 = @6521
+  IF ~~ THEN DO ~SetGlobal("MO_AidDeathbringer","GLOBAL",1)~ GOTO 3
+END
+
+IF ~~ THEN BEGIN 5
+  SAY @6530
+  IF ~~ THEN GOTO 3
+END
+
+IF ~~ THEN BEGIN 6
+  SAY @6540
+  IF ~~ THEN GOTO 3
+END
+
+IF ~~ THEN BEGIN 7
+  SAY @6550
+  IF ~~ THEN GOTO 3
+END
+
+IF ~~ THEN BEGIN 8
+  SAY @6560
+  IF ~~ THEN GOTO 3
+END
