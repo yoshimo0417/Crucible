e88e405fa155a8a89f1272d6a546a917089a7e95
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-11 23:29:52 -0700

    Caldus Dialog

--- crucible/dlg/mobha37.d
+++ crucible/dlg/mobha37.d
@@ -5,8 +5,72 @@
 BEGIN MOBHA37
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
-  SAY @6400
+  SAY @6400 
+  IF ~IsValidForPartyDialog("IMOEN2")~ THEN GOTO 1
+  IF ~!IsValidForPartyDialog("IMOEN2")~ THEN GOTO 3
+END
+
+IF ~~ THEN BEGIN 1
+  SAY @6401
+  IF ~~ THEN EXTERN IMOEN25J 2
+END 
+
+IF ~~ THEN BEGIN 3
+  SAY @6403
+  IF ~IsValidForPartyDialog("SAREVOK")~ THEN GOTO 4
+  IF ~!IsValidForPartyDialog("SAREVOK")~ THEN GOTO 7
+END
+
+IF ~~ THEN BEGIN 4
+  SAY @6404
+  IF ~~ EXTERN SAREV25J 5
+END
+
+IF ~~ THEN BEGIN 6
+  SAY @6406
+  IF ~~ THEN GOTO 7
+END
+
+IF ~~ THEN BEGIN 7
+  SAY @6407
+  IF ~Alignment(Player1,MASK_EVIL)~ THEN REPLY @6408 GOTO 8
+  IF ~~ THEN REPLY @6409 GOTO 9
+  IF ~~ THEN REPLY @6410 GOTO 9
+  IF ~~ THEN REPLY @6411 GOTO 10
+END
+
+IF ~~ THEN BEGIN 8
+  SAY @6413
+  IF ~~ THEN REPLY @6409 GOTO 9
+  IF ~~ THEN REPLY @6410 GOTO 9
+  IF ~~ THEN REPLY @6411 GOTO 10
+END
+
+IF ~~ THEN BEGIN 9
+  SAY @6414
+  IF ~~ THEN GOTO 10
+END
+
+IF ~~ THEN BEGIN 10
+  SAY @6412
   IF ~~ THEN DO ~SetGlobal("MOBHA2_Enemy","GLOBAL",1)
 Enemy()~ EXIT
 END
 
+//-------------------------------------------------------------------------
+// Imoen External
+APPEND IMOEN25J
+  IF ~~ THEN BEGIN 2
+    SAY @6402
+    IF ~~ THEN EXTERN MOBHA37 3
+  END
+END
+
+//-------------------------------------------------------------------------
+// Sarevok External
+APPEND SAREV25J
+  IF ~~ THEN BEGIN 5
+    SAY @6405
+    IF ~~ THEN EXTERN MOBHA37 6
+  END
+END
