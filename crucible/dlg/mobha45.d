54ca0947ef170e5bbf07b897ddcf12ff6d9c30c7
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-15 23:03:42 -0700

    Update mobha45.d

--- crucible/dlg/mobha45.d
+++ crucible/dlg/mobha45.d
@@ -7,10 +7,10 @@ BEGIN MOBHA45
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
   SAY @5301
   IF ~~ THEN DO ~Enemy()
-CreateCreatureEffect("mobha12","SPSPEV",[589.1062],ESE)
-CreateCreatureEffect("mobha12","SPSPEV",[687.972],ESE)
-CreateCreatureEffect("mobha12","SPSPEV",[559.981],ESE)
-CreateCreatureEffect("mobha12","SPSPEV",[474.1055],ESE)
-CreateCreatureEffect("mobha12","SPSPEV",[658.920],ESE)~ EXIT
+CreateCreatureEffect("mobha12","SPSPEV",[589.1062],SE)
+CreateCreatureEffect("mobha12","SPSPEV",[687.972],SE)
+CreateCreatureEffect("mobha12","SPSPEV",[559.981],SE)
+CreateCreatureEffect("mobha12","SPSPEV",[474.1055],SE)
+CreateCreatureEffect("mobha12","SPSPEV",[658.920],SE)~ EXIT
 END
 
