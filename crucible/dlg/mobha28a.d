2ee6089833ce89b551f60e62a78a97a92bcfac3d
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-18 23:42:53 -0700

    Uthaedeol Summon Dialog

--- crucible/dlg/mobha28a.d
+++ crucible/dlg/mobha28a.d
@@ -1,12 +1,21 @@
 ///////////////////////////////////////////////////////////////////
-// Dialogue for Malchediel
+// Dialogue for Malchediel (Entrance)
 // Solar in charge of the Planetar Garrison
 ///////////////////////////////////////////////////////////////////
-BEGIN MOBHA28
+BEGIN MOBHA28A
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
-  SAY @6200 = @6201
-  IF ~~ THEN DO ~GiveItemCreate("mobhabl2",Player1,1,0,0)
-SetGlobal("MOBHA8_Enemy","GLOBAL",1)
-ForceSpell(Myself,DRYAD_TELEPORT)~ EXIT
+  SAY @6210
+  IF ~~ THEN REPLY @6211 GOTO 1  // I have no choice but to press forward.
+  IF ~~ THEN REPLY @6212 GOTO 1  // I am not a gambler and care little for odds.
+  IF ~~ THEN REPLY @6213 GOTO 1  // Nothing will stop me from my destiny.
+  IF ~~ THEN REPLY @6214 GOTO 1  // I shall crush any who stand in my way.
+END
+
+IF ~~ THEN BEGIN 1
+  SAY @6215 = @6216 = @6217 = @6218
+  IF ~~ THEN DO ~ClearAllActions()
+SmallWait(5)
+StartCutSceneMode()
+StartCutScene("mocut07")~ EXIT
 END
--- crucible/dlg/mobha44.d
+++ crucible/dlg/mobha44.d
@@ -6,6 +6,33 @@ BEGIN MOBHA44
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
   SAY @6600
-  IF ~~ THEN DO ~~ EXIT
+  IF ~~ THEN EXTERN MOBHA28A MOMALCHEDIEL1
 END
 
+IF ~~ THEN BEGIN 1
+  SAY @6602 = @6603
+  IF ~~ THEN EXTERN MOBHA28A MOMALCHEDIEL2
+END
+
+IF ~Global("MO_UthaedeolChat","MOBHA8",1)~ THEN BEGIN 2
+  SAY @6606
+  IF ~~ THEN DO ~SetGlobal("MO_UthaedeolChat","MOBHA8",2)~ EXIT
+END
+
+//-------------------------------------------------------------------------
+// Malchediel External
+APPEND MOBHA28A
+  IF ~~ THEN BEGIN MOMALCHEDIEL1
+    SAY @6601
+    IF ~~ THEN EXTERN MOBHA44 1
+  END
+
+  IF ~~ THEN BEGIN MOMALCHEDIEL2
+    SAY @6604 = @6605
+    IF ~~ THEN DO ~ClearAllActions()
+SetGlobal("MO_UthaedeolChat","MOBHA8",1)
+SmallWait(5)
+StartCutSceneMode()
+StartCutScene("mocut08")~ EXIT
+  END
+END
