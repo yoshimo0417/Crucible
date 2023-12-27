5a336d82467c3ccf4f8c104b6f7f814df9679cf0
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2023-12-26 23:16:00 -0700

    Lotta cutscene stuff

--- crucible/dlg/mobha46.d
+++ crucible/dlg/mobha46.d
@@ -6,6 +6,26 @@ BEGIN MOBHA46
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
   SAY @6700
+  IF ~~ THEN REPLY @6702 GOTO 1
+  IF ~~ THEN REPLY @6703 GOTO 1
+  IF ~~ THEN REPLY @6704 GOTO 1
+  IF ~~ THEN REPLY @6705 GOTO 1
+END
+
+IF ~~ THEN BEGIN 1
+  SAY @6706
+  IF ~~ THEN DO ~ClearAllActions()
+StartCutSceneMode()
+StartCutScene("mocut09")~ EXIT
+END
+
+IF ~Global("MO_LichConvo","MOBHA0",0)~ THEN BEGIN 2
+  SAY @6707 = @6708
+  IF ~~ THEN DO ~SetGlobal("MO_LichConvo","MOBHA0",1)~ EXIT
+END
+
+IF ~Global("MO_LichConvo","MOBHA0",1)~ THEN BEGIN 3
+  SAY @6709 
   IF ~~ THEN DO ~~ EXIT
 END
 
