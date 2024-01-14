73536227f4e2fc248674bfee9f91597628a383b8
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2024-01-13 23:12:49 -0700

    Solar Defeat Cleanup

--- crucible/dlg/mobha28.d
+++ crucible/dlg/mobha28.d
@@ -9,3 +9,19 @@ IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
   IF ~~ THEN DO ~SetGlobal("MOBHA9_Enemy","GLOBAL",1)
 Enemy()~ EXIT
 END
+
+IF ~HPLT(Myself,2)~ THEN BEGIN 1
+  SAY @6220
+  IF ~~ THEN DO ~SetGlobal("MO_Malchediel_Defeated","GLOBAL",1)
+AddXPObject(Player1,20000)
+AddXPObject(Player2,20000)
+AddXPObject(Player3,20000)
+AddXPObject(Player4,20000)
+AddXPObject(Player5,20000)
+AddXPObject(Player6,20000)
+ChangeEnemyAlly(Myself,NEUTRAL)
+GiveItemCreate("mobhabl2",Player1,1,0,0)
+ClearAllActions()
+StartCutSceneMode()
+StartCutScene("mocut20")~ EXIT
+END
