536082ffbd69b71900ba08d21f71c2f9474e6714
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2024-01-19 22:21:02 -0700

    Jambi

--- crucible/dlg/mobha53.d
+++ crucible/dlg/mobha53.d
@@ -1,16 +1,44 @@
 ///////////////////////////////////////////////////////////////////
-// Dialogue for Mercane
+// Dialogue for Jambi
 // Shopkeep and Innkeeper in the Main Area
 ///////////////////////////////////////////////////////////////////
 BEGIN MOBHA53
 
-IF ~True()~ THEN BEGIN 0
-  SAY @7100
-  IF ~~ THEN REPLY @7100 DO ~StartStore("mobha53",LastTalkedToBy())~ EXIT // Yes
-  IF ~~ THEN REPLY @7102 GOTO 1 // No
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
+  SAY @7110 = @7111
+  IF ~~ THEN REPLY @7112 GOTO 4  // What is this place?
+  IF ~~ THEN REPLY @7113 GOTO 3  // Who are you?
+  IF ~~ THEN REPLY @7114 GOTO 4  // Why is your sanctuary out here?
+  IF ~~ THEN REPLY @7115 GOTO 1  // I have no desire to speak with you.
 END
 
+
 IF ~~ THEN BEGIN 1
+  SAY @7116
+  IF ~~ THEN GOTO 2
+END
+
+IF ~~ THEN BEGIN 2
   SAY @7103
   IF ~~ THEN DO ~~ EXIT
+END
+
+IF ~~ THEN BEGIN 3
+  SAY @7117 = @7118
+  IF ~~ THEN GOTO 5
+END 
+
+IF ~~ THEN BEGIN 4
+  SAY @7120
+  IF ~~ THEN GOTO 5
+END 
+
+IF ~!NumTimesTalkedTo(0)~ THEN BEGIN 5
+  SAY @7119
+  IF ~~ THEN REPLY @7121 DO ~StartStore("mobha53",LastTalkedToBy())~ EXIT // I need a safe place to lay my head.
+  IF ~~ THEN REPLY @7122 DO ~StartStore("mobha53",LastTalkedToBy())~ EXIT // Let me see your store.
+  IF ~~ THEN REPLY @7112 GOTO 4  // What is this place?
+  IF ~~ THEN REPLY @7113 GOTO 3  // Who are you?
+  IF ~~ THEN REPLY @7114 GOTO 4  // Why is your sanctuary out here?
+  IF ~~ THEN REPLY @7123 GOTO 2  // I'm good for now.
 END
