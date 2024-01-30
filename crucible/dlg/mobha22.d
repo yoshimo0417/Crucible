67d6ecec56c9e8de9a6012dea967cb61d7161832
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2024-01-29 23:17:02 -0700

    Entropy Reaper Correction

--- crucible/dlg/mobha22.d
+++ crucible/dlg/mobha22.d
@@ -81,7 +81,7 @@ IF ~Global("MO_KirinQuest","GLOBAL",2)~ THEN BEGIN 12
   SAY @7225
   IF ~~ THEN DO ~ForceSpell(Myself,FLASHY_2)
 SetGlobal("MO_KirinQuest","GLOBAL",3)
-SetGlobalTimer("MO_KirinQuest","GLOBAL",20)
+SetGlobalTimer("MO_KirinQuest","GLOBAL",THREE_ROUNDS)
 CreateVisualEffectObject("SPPLANAR","mobha22")
 Deactivate(Myself)~ EXIT
 END
